94db82c3818ee4ae569cddf29329b71fedb3a4d4
Kraete <kraete@BallsDeep.(none)>
2014-01-17 15:45:44 -0500

    Updated motor init

--- Senior Project/Flight/motor_init.ino
+++ Senior Project/Flight/motor_init.ino
@@ -5,24 +5,30 @@ Servo fl;
 Servo rr;
 Servo rl;
 
+int _throttle = A15;
+int _speed;
 void setup(){
-  
+  Serial.begin(9600);
+  pinMode(_throttle, INPUT);
   fr.attach(40);
-  fl.attach(41);
+  rl.attach(41);
   rr.attach(42);
-  rl.attach(43);
+  fl.attach(43);
   fr.writeMicroseconds(1000);
   fl.writeMicroseconds(1000);
   rr.writeMicroseconds(1000);
   rl.writeMicroseconds(1000);
-  delay(3000);
+  delay(10000);
 }
 
 void loop(){
-  fr.writeMicroseconds(2000);
-  fl.writeMicroseconds(2000);
-  rr.writeMicroseconds(2000);
-  rl.writeMicroseconds(2000);
+  _speed = analogRead(_throttle);
+  _speed = map(_speed, 0, 1023, 1000, 2000);
+  Serial.println(_speed);
+  fr.writeMicroseconds(_speed);
+  fl.writeMicroseconds(_speed);
+  rr.writeMicroseconds(_speed);
+  rl.writeMicroseconds(_speed);
 }
 
 
